a563e22392066f19d2dbcbe1355b3fbe85df551a
jabbyy <jeppem65@gmail.com>
2024-06-06 01:25:45 +0200

    Created the encryption class

---
+++ Codes/AESEncryption.cs
@@ -0,0 +1,127 @@
+﻿namespace Svendeprøve_projekt_BodyFitBlazor.Codes
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+
+    public class AESEncryption
+    {
+        private byte[] key;
+
+        public AESEncryption(byte[] encryptionKey)
+        {
+            key = encryptionKey;
+        }
+
+        public string EncryptString(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = new byte[16]; // Initialization Vector
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                byte[] encrypted;
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    encrypted = msEncrypt.ToArray();
+                }
+
+                return Convert.ToBase64String(encrypted);
+            }
+        }
+
+        public string DecryptString(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = new byte[16]; // Initialization Vector
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+
+                string plaintext = null;
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherTextBytes))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+                return plaintext;
+            }
+        }
+
+        public byte[] EncryptNumber(int number)
+        {
+            byte[] numberBytes = BitConverter.GetBytes(number);
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = new byte[16]; // Initialization Vector
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                byte[] encrypted;
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(numberBytes, 0, numberBytes.Length);
+                    }
+
+                    encrypted = msEncrypt.ToArray();
+                }
+
+                return encrypted;
+            }
+        }
+
+        public int DecryptNumber(byte[] encryptedNumber)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = new byte[16]; // Initialization Vector
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                byte[] decrypted;
+
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedNumber))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (MemoryStream msPlain = new MemoryStream())
+                        {
+                            csDecrypt.CopyTo(msPlain);
+                            decrypted = msPlain.ToArray();
+                        }
+                    }
+                }
+
+                return BitConverter.ToInt32(decrypted, 0);
+            }
+        }
+    }
+
+}
